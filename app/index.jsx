82180b32426339070b5f6f6981081671d62666e4
Sam Kamble <kamble.samarth91@gmail.com>
2024-08-10 23:03:48 +0530

    Implement or Intilize app and setup native wind

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-red-200">
+      <Text className="text-3xl font-pblack">Aora!</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile">Profile</Link>
+    </View>
+  );
+}
