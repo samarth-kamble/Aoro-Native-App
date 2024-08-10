cc23792e21b7a2fa793fe2885cff9558a162a743
Sam Kamble <kamble.samarth91@gmail.com>
2024-08-11 00:21:39 +0530

    Implement Folder Struture and bottom tab nav '

--- app/index.jsx
+++ app/index.jsx
@@ -7,7 +7,7 @@ export default function App() {
     <View className="flex-1 items-center justify-center bg-red-200">
       <Text className="text-3xl font-pblack">Aora!</Text>
       <StatusBar style="auto" />
-      <Link href="/profile">Profile</Link>
+      <Link href="/home">Home</Link>
     </View>
   );
 }
